40b73139a812c8ef8e024e4b6826c8a1c16d0b01
sam <syairsuryasamudra24@gmail.com>
2025-08-11 15:10:55 +0700

    new update

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,16 @@ fetch("navbar.html")
     document.getElementById("navbar-placeholder").innerHTML = data;
   });
 // javascript navbar end
+
+// shadow buttop nav
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// shadow buttop nav end
